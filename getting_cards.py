a3edaa7dd77bcae909c34e0a41d666fa00ca95ef
sdragonslayer <sydu2021@gmail.com>
2025-09-23 22:19:03 -0400

    relevant cards in json

--- getting_cards.py
+++ getting_cards.py
@@ -1,6 +1,13 @@
 import json 
 
-with open('clash_royale_cards.json', 'r') as clash:
+with open('clash_royale_cards_full.json', 'r') as clash:
     data = json.load(clash)
 
-right_cards = {}
+cards = data["cards"]
+
+filtered = [card for card in cards if card["arena"] <= 1]
+
+low_arena_cards = {"cards": filtered}
+
+with open("cards.json", "w") as new:
+    json.dump(low_arena_cards, new, indent=4)
