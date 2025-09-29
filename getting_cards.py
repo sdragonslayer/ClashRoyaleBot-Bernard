929f7eb3f5e433fe05c68d4582086a1f4c2f05c6
sdragonslayer <bd569151@703-17-M1.local>
2025-09-29 12:20:53 -0400

    new cards, no gob cage or hut

--- getting_cards.py
+++ getting_cards.py
@@ -1,13 +1,31 @@
 import json 
 
-with open('clash_royale_cards_full.json', 'r') as clash:
+with open('troops.json', 'r') as clash:
     data = json.load(clash)
 
-cards = data["cards"]
+troops = data["troops"]
 
-filtered = [card for card in cards if card["arena"] <= 1]
+filtered_troops = [troop for troop in troops if troop["arena"] <= 1]
 
-low_arena_cards = {"cards": filtered}
+with open('spells.json', 'r') as clash1:
+    data1 = json.load(clash1)
+
+spells = data1["spells"]
+
+filtered_spells = [spell for spell in spells if spell["arena"] <= 1]
+
+with open('towertroops.json', 'r') as clash2:
+    data2 = json.load(clash2)
+
+towers = data2["tower_troops"]
+
+filtered_towers = [tower for tower in towers if tower["arena"] <= 1]
+
+low_arena_cards = {
+    "troops": filtered_troops,
+    "spells": filtered_spells,
+    "towers": filtered_towers
+}
 
 with open("cards.json", "w") as new:
     json.dump(low_arena_cards, new, indent=4)
