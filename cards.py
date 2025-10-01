e27b3b1ac6ee2489ed0097835c2b00372b436aa2
sdragonslayer <bd569151@703-17-M1.local>
2025-10-01 12:45:27 -0400

    classes

--- cards.py
+++ cards.py
@@ -1,3 +1,32 @@
-class Cards:
-    def __init__(self, hp, damage, ):
-        pass
+class Troop:
+    def __init__(self, card_data, location):
+        self.elixer = card_data["elixer"]
+        self.hp = card_data["combat_stats"]["hitpoints"]["11"]
+        self.dps = card_data["combat_stats"]["damage"]["11"] / card_data["mechanics"]["hit_speed"]["11"]        
+        # tiles / second
+        self.speed = card_data["mechanics"]["speed"] / 60
+        self.location = location
+        self.counters = card_data["counters"]
+        self.synergies = card_data["synergies"]    
+
+class Spell:
+    def __init__(self, card_data, location):
+        self.elixir = card_data["elixir"]
+        self.damage = card_data["combat_stats"]["damage"]["11"]
+        self.crown_tower_damage = card_data["combat_stats"]["crown_tower_damage"]["11"]
+        self.radius = card_data["mechanics"]["radius"]
+        self.location = location
+        self.knockback = "Knockback" in card_data["special_abilities"]
+        self.counters = card_data["counters"]
+        self.synergies = card_data["synergies"]
+
+class Tower:
+    # initating defaults of everything
+    def __init__(self, card_data, location):
+        self.hp = card_data["combat_stats"]["hitpoints"]["11"]
+        self.dps = card_data["combat_stats"]["damage"]["11"] / card_data["mechanics"]["hit_speed"]["11"]
+        self.range = card_data["mechanics"]["range"]["11"]
+        self.tower_fallen = False 
+        self.location = location
+
+
--- loading_troops/getting_cards.py
+++ loading_troops/getting_cards.py
@@ -5,14 +5,30 @@ with open('troops.json', 'r') as clash:
 
 troops = data["troops"]
 
-filtered_troops = [troop for troop in troops if troop["arena"] <= 1]
+sckeys_to_find = [
+    "Minions",
+    "Archers",
+    "Knight",
+    "MiniPekka",
+    "Musketeer",
+    "Giant",
+    "Spear Goblins",
+    "Goblins"
+]
+
+filtered_troops = [troop for troop in troops if troop.get("sc_key") in sckeys_to_find]
 
 with open('spells.json', 'r') as clash1:
     data1 = json.load(clash1)
 
 spells = data1["spells"]
 
-filtered_spells = [spell for spell in spells if spell["arena"] <= 1]
+spells_to_find = [
+    "Fireball",
+    "Arrows"
+]
+
+filtered_spells = [spell for spell in spells if spell.get("sc_key") in spells_to_find]
 
 with open('towertroops.json', 'r') as clash2:
     data2 = json.load(clash2)
